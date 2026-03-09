333c4f7bd96b77c125b6ed418c2956cca7c459bd
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-03-09 08:49:28 -0300

    Add initial HTML structure to index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>
+                    <a href="index.php">Home</a>
+                </td>
+                <td>
+                    <a href="tabelausuario.php">Tabela usuário</a>
+                </td>
+                <td>
+                    <a href="tabelacategoria.php">Tabela categoria</a>
+                </td>
+                <td>
+                    <a href="tabelavenda.php">Tabela venda</a>
+                </td>
+                <td>
+                    <a href="tabelaproduto.php">Tabela produto</a>
+                </td>
+                <td>
+                    <a href="tabelacliente.php">Tabela cliente</a>
+                </td>
+            </tr>
+        </table>
+        <h1>Loja WEB 3</h1>
+        <div>
+            <div>
+                <a href="tabelausuario.php">
+                    <button>Tabela usuário</button>
+                </a>
+            </div>
+            <div>
+                <a href="tabelacategoria.php">
+                    <button>Tabela categoria</button>
+                </a>
+            </div>
+            <div>
+                <a href="tabelavenda.php">
+                    <button>Tabela venda</button>
+                </a>
+            </div>
+            <div>
+                <a href="tabelaproduto.php">
+                    <button>Tabela produto</button>
+                </a>
+            </div>
+            <div>
+                <a href="tabelacliente.php">
+                    <button>>Tabela cliente</button>
+                </a>
+            </div>
+            <div>
+                <a href="relatorio.php">
+                    <button>>Relatório</button>
+                </a>
+            </div>
+        </div>
+    </body>
+
+</html>
