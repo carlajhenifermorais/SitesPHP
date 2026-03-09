96da420d19389762e65383a2becabc2c8d4cc0af
Carla Jhenifer Morais Vieira <89932530+carlajhenifermorais@users.noreply.github.com>
2026-03-09 08:49:03 -0300

    Add relatorio.php with navigation links and header

---
+++ relatorio.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>
+                    <a href="index.php">Home</a>
+                </td>
+                <td>
+                    <a href="tabelausuario.php">Tabela usuário</a>
+                </td>
+                <td>
+                    <a href="tabelacategoria.php">Tabela categoria</a>
+                </td>
+                <td>
+                    <a href="tabelavenda.php">Tabela venda</a>
+                </td>
+                <td>
+                    <a href="tabelaproduto.php">Tabela produto</a>
+                </td>
+                <td>
+                    <a href="tabelacliente.php">Tabela cliente</a>
+                </td>
+            </tr>
+        </table>
+        <h1>Relatório</h1>
+    </body>
+
+</html>
